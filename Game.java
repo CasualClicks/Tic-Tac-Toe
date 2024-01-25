acd089f5c4de0a88f81bfde10c2ecdd1a10492ed
Shivam Singhal <shivamsinghal22.ss@gmail.com>
2024-01-26 00:38:47 +0530

    Commit - 3 - Complete Code for Tic-Tac-Toe problem
    Added the minimax function, that deals with the main logic by using backtracking to find optimal solution.
    
    Now, I just have to add appropriate comments and improve readability.

--- Game.java
+++ Game.java
@@ -5,8 +5,17 @@ class Game {
     private static final int MACHINE = 0;
     private static final int USER = 1;
     private static final int SIDE = 3;
-    private static final char MachineMove = 'X';
-    private static final char UserMove = 'O';
+    private static final char MachineMove = 'O';
+    private static final char UserMove = 'X';
+
+    public static void showBoard(char board[][]) {
+        // using printf function to increase readability
+        System.out.printf("\t\t\t %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
+        System.out.printf("\t\t\t ------------\n");
+        System.out.printf("\t\t\t %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
+        System.out.printf("\t\t\t ------------\n");
+        System.out.printf("\t\t\t %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
+    }
 
     public static void Instructions() {
         System.out.println("\nChoose a cell number from 1 to 9 as below and play\n");
@@ -57,6 +66,60 @@ class Game {
             return true;
 
         return false;
+    }
+
+    // MAIN ALGORITHM
+    // logic - we basically assign scores to conditions. If 'X' wins ->+10, if 'O'
+    // wins->-10, else draw -> 0
+    // it is a backtracking algorithm. We basically would be tracking back from the
+    // very final solution and would choose that path back to root node, where score
+    // is maximum.
+    // here, isAI boolean value states whether it is computer's chance or not
+    public static int minimax(char[][] board, int depth, boolean isAI) {
+        int score = 0;
+        int maxScore = 0;
+
+        if (gameOver(board) == true) {
+            if (isAI == true)
+                return -10;
+            if (isAI == false)
+                return +10;
+        } else {
+            if (depth < 9) {
+                if (isAI == true) {
+                    maxScore = Integer.MIN_VALUE;
+                    for (int i = 0; i < SIDE; i++) {
+                        for (int j = 0; j < SIDE; j++) {
+                            if (board[i][j] == '*') {
+                                board[i][j] = MachineMove;
+                                score = minimax(board, depth + 1, false);
+                                board[i][j] = '*';
+                                if (score > maxScore)
+                                    maxScore = score;
+                            }
+                        }
+                    }
+                    return maxScore; // Fix here
+                } else {
+                    maxScore = Integer.MAX_VALUE;
+                    for (int i = 0; i < SIDE; i++) {
+                        for (int j = 0; j < SIDE; j++) {
+                            if (board[i][j] == '*') {
+                                board[i][j] = UserMove;
+                                score = minimax(board, depth + 1, true);
+                                board[i][j] = '*';
+                                if (score < maxScore)
+                                    maxScore = score;
+                            }
+                        }
+                    }
+                    return maxScore;
+                }
+            } else {
+                return 0;
+            }
+        }
+        return 0;
     }
 
     // this funciton would be responsible for calculating machine's best move, after
@@ -110,7 +173,7 @@ class Game {
                 x = n / SIDE;
                 y = n % SIDE;
                 board[x][y] = MachineMove;
-                System.out.println("\nMachine marked " + MachineMove + " in cell " + n + 1);
+                System.out.println("\nMachine marked " + MachineMove + " in cell " + (n + 1));
                 showBoard(board);
                 moveIndex++;
                 turn = USER;
@@ -120,8 +183,8 @@ class Game {
                 for (int i = 0; i < SIDE; i++)
                     for (int j = 0; j < SIDE; j++)
                         if (board[i][j] == '*')
-                            System.out.print(i * 3 + j + 1); // converting index to posiiton
-                System.out.print("\nEnter the posiiton: ");
+                            System.out.print(((i * 3 + j) + 1) + " "); // converting index to posiiton
+                System.out.print("\nEnter the position: ");
                 n = in.nextInt();
                 n--; // because we are using 0-based indexing and user input is in 1-based indexing
                 x = n / SIDE; // calculating row on the basis of input
@@ -130,7 +193,7 @@ class Game {
                 // if entry is valid
                 if (board[x][y] == '*' && n < 9 && n >= 0) {
                     board[x][y] = UserMove;
-                    System.out.println("\nUser marked " + UserMove + " in cell " + n + 1);
+                    System.out.println("\nUser marked " + UserMove + " in cell " + (n + 1));
                     showBoard(board);
                     moveIndex++;
                     turn = MACHINE;
@@ -154,7 +217,7 @@ class Game {
         else {
             turn = (turn == USER) ? MACHINE : USER; // this is done because if a party wins, we won't know that until
                                                     // next iteration where the turn is now given to opposite party
-            declareWinner(turn);
+            Winner(turn);
         }
     }
 
