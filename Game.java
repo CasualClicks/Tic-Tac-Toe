ef8094ed9c8c6d217cd192014eeb54fb211f87a3
Shivam Singhal <shivamsinghal22.ss@gmail.com>
2024-01-25 20:44:10 +0530

    Commit-1 - Wrote the code for basic UI and human logic
    For this commit, I have written the code to display tic-tac-toe board onto the console, and coded for human responses to mark the board.
    
    Next commit, we would be coding to add logic to machine, so that it plays optimally (using backtracking)

---
+++ Game.java
@@ -0,0 +1,160 @@
+import java.util.*;
+import java.lang.*;
+
+class Game {
+    private static final int MACHINE = 0;
+    private static final int USER = 1;
+    private static final int SIDE = 3;
+    private static final char MachineMove = 'X';
+    private static final char UserMove = 'O';
+
+    public static void Instructions() {
+        System.out.println("\nChoose a cell number from 1 to 9 as below and play\n");
+        System.out.println("\t\t\t 1 | 2 | 3 ");
+        System.out.println("\t\t\t ---------");
+        System.out.println("\t\t\t 4 | 5 | 6 ");
+        System.out.println("\t\t\t ---------");
+        System.out.println("\t\t\t 7 | 8 | 9 \n");
+    }
+
+    public static void Initialize(char[] board[]) {
+        for (int i = 0; i < SIDE; i++)
+            for (int j = 0; j < SIDE; j++)
+                board[i][j] = '*';
+    }
+
+    public static void Winner(int turn) {
+        if (turn == MACHINE)
+            System.out.println("MACHINE has won\n");
+        else
+            System.out.println("USER has won\n");
+    }
+
+    public static boolean gameOver(char board[][]) {
+        return (rowCrossed(board) || columnCrossed(board) || diagonalCrossed(board));
+    }
+
+    public static boolean rowCrossed(char board[][]) {
+        for (int i = 0; i < SIDE; i++)
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '*')
+                return true;
+        return false;
+    }
+
+    public static boolean columnCrossed(char board[][]) {
+        for (int i = 0; i < SIDE; i++)
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != '*')
+                return true;
+        return false;
+    }
+
+    public static boolean diagonalCrossed(char board[][]) {
+        // diagonal from top left to bottom right
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '*')
+            return true;
+        // diagonal from top right to bottom left
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '*')
+            return true;
+
+        return false;
+    }
+
+    public static void TicTacToe(int turn) {
+        // Initializing a Scanner class object for taking inputs
+        Scanner in = new Scanner(System.in);
+
+        // initializing a 9X9 board
+        char board[][] = new char[SIDE][SIDE];
+        int moveIndex = 0, x = 0, y = 0;
+
+        Initialize(board);
+        Instructions();
+
+        // until game is over or moveIndex goes over the bounds
+        while (gameOver(board) == false && moveIndex != SIDE * SIDE) {
+            int n; // this temp variable would be used to store the posiiton where the move is
+                   // played.
+            if (turn == MACHINE) {
+                // here, i would be later coding for machine logic, where my machine would be
+                // playing optimally
+            } else if (turn == USER) {
+                // printing the open positions, where the user can play it's move
+                System.out.print("\nYou can insert in the following positions: ");
+                for (int i = 0; i < SIDE; i++)
+                    for (int j = 0; j < SIDE; j++)
+                        if (board[i][j] == '*')
+                            System.out.print(i * 3 + j + 1); // converting index to posiiton
+                System.out.print("\nEnter the posiiton: ");
+                n = in.nextInt();
+                n--; // because we are using 0-based indexing and user input is in 1-based indexing
+                x = n / SIDE; // calculating row on the basis of input
+                y = n % SIDE; // calculating column on the basis of input
+
+                // if entry is valid
+                if (board[x][y] == '*' && n < 9 && n >= 0) {
+                    board[x][y] = UserMove;
+                    System.out.println("\nUser marked " + UserMove + " in cell " + n + 1);
+                    showBoard(board);
+                    moveIndex++;
+                    turn = MACHINE;
+                }
+                // else if user gives input location which is already occupied
+                else if (board[x][y] != '*' && n < 9 && n >= 0) {
+                    System.out.println("\nAlready occupied posiiton. Please select a valid position");
+                }
+                // else if user inputs cell value out of range
+                else if (n < 0 || n >= 9) {
+                    System.out.println("\nCell Range Invalid / Cell range out of bounds.");
+                }
+            }
+        }
+
+        // if the game is not over, but i have moved out of cell range (all cells
+        // filled)
+        if (gameOver(board) == false && moveIndex == SIDE * SIDE)
+            System.out.println("\nIt's a draw !!");
+        // if gammeOver(board)==true
+        else {
+            turn = (turn == USER) ? MACHINE : USER; // this is done because if a party wins, we won't know that until
+                                                    // next iteration where the turn is now given to opposite party
+            declareWinner(turn);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Calling Scanner class to take inputs
+        Scanner in = new Scanner(System.in);
+
+        // Using print streams for UI
+        System.out.println("\n----------------------------------------------------------------\n");
+        System.out.println("\t\t\t Tic-Tac-Toe\n");
+        System.out.println("----------------------------------------------------------------\n");
+
+        char want_to_continue = 'y';
+        while (want_to_continue == 'y') {
+            char choice;
+            System.out.print("Would you like to start first(y/n): ");
+            choice = Character.toLowerCase(in.nextLine().charAt(0));
+
+            switch (choice) {
+                case 'n':
+                    // System.out.println("\nPlaying tic tac toe machine first :)");
+                    TicTacToe(MACHINE);
+                    break;
+                case 'y':
+                    // System.out.println("\nPlaying tic tac toe :)");
+                    TicTacToe(USER);
+                    break;
+                default:
+                    System.out.println("\nInvalid Choice\n");
+            }
+
+            System.out.print("\nDo you want to try again(y/n): ");
+            want_to_continue = Character.toLowerCase(in.nextLine().charAt(0));
+            System.out.println();
+
+            in.close();
+        }
+    }
+}
